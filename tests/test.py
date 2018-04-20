7ddc3528fc79ef1ee11ac152bab1679bce8193bc
gafetinov <artem.gafetinov@gmail.com>
2018-04-21 00:56:13 +0500

    Add files via upload
    add tests and debug

--- tests/test.py
+++ tests/test.py
@@ -1,12 +1,94 @@
 import validator
-import pytest
 
 
-class TestValidator:
+class TestSearch():
+    def test_of_identation(self):
+        code = self.get_list('identation_test.txt')
+        answer = validator.search_errors(code)[0].get_description()
+        assert answer == 'Indentation is not a multiple of four'
 
-    def test_type_error(self):
-        with pytest.raises(TypeError):
-            list(validator.Error((1, 1), 'Indentation is not a multiple of four'))
+    def test_imports(self):
+        code = self.get_list('import_test.txt')
+        answer = validator.search_errors(code)[0].get_description()
+        assert answer == 'Every import should be on a separate line'
 
-    def test_identation(self):
-        assert [validator.Error((1, 1), 'Indentation is not a multiple of four')] == validator.search_errors(' class Super:\n')
+    def test_function_names(self):
+        code = self.get_list('test_function_name')
+        answer = validator.search_errors(code)
+        for i in range(2):
+            assert answer[i].get_description() == 'Wrong name of function'
+        assert len(answer) == 2
+
+    def test_class_names(self):
+        code = self.get_list('test_class_name')
+        answer = validator.search_errors(code)
+        for i in range(3):
+            assert answer[i].get_description() == 'Wrong name of class'
+        assert len(answer) == 3
+
+    def test_spaces_around_parameter(self):
+        code = self.get_list('test_spaces_around_parameter')
+        answer = validator.search_errors(code)[0].get_description()
+        assert answer == 'Unexpected spaces around keyword / parameter equals'
+
+    def test_long_line(self):
+        code = self.get_list('long_line')
+        answer = validator.search_errors(code)
+        for i in range(2):
+            assert answer[i].\
+                       get_description() == 'Symbols in line over than 79'
+        assert len(answer) == 2
+
+    def test_spaces_around_brackets(self):
+        code = self.get_list('test_spaces_around_brackets')
+        answer = validator.search_errors(code)
+        descriptions = []
+        for error in answer:
+            descriptions.append(error.get_description())
+        assert descriptions[0] == 'whitespace after ('
+        assert descriptions[1] == 'whitespace after ['
+        assert descriptions[2] == 'whitespace before ]'
+        assert descriptions[3] == 'whitespace after {'
+        assert descriptions[4] == 'whitespace before }'
+        assert descriptions[5] == 'whitespace before )'
+
+    def test_spaces_before_punctuation(self):
+        code = self.get_list('test_spaces_before_punctuation')
+        answer = validator.search_errors(code)
+        descriptions = []
+        for error in answer:
+            descriptions.append(error.get_description())
+        assert descriptions[0] == 'whitespace before :'
+        assert descriptions[1] == 'whitespace before ,'
+        assert descriptions[2] == 'whitespace before ;'
+
+    def test_multiple_statements(self):
+        code = self.get_list('multiple_statements')
+        answer = validator.search_errors(code)
+        for i in range(2):
+            assert answer[i].\
+                       get_description() == 'multiple statements on one line'
+        assert len(answer) == 2
+
+    def test_comments(self):
+        code = self.get_list('comments')
+        answer = validator.search_errors(code)
+        for i in range(2):
+            assert answer[i].\
+                get_description() == 'inline comment should start with "# "'
+        assert len(answer) == 2
+
+    def test_logical_conditions(self):
+        code = self.get_list('logical_conditions')
+        answer = validator.search_errors(code)
+        for i in range(6):
+            assert answer[i].\
+                get_description() == 'Do not compare logical types through operators "==", "!=", "is", "is not"'
+        assert len(answer) == 6
+
+    def get_list(self, file):
+        code = []
+        with open(file) as content:
+            for line in content:
+                code.append(line)
+        return code
--- validator.py
+++ validator.py
@@ -1,6 +1,6 @@
-bin_operators = ('=', '+=', '-=', '*=', '/=',
-                 '==', '<', '>', '<=', '>=',
-                 'in', 'not in', 'is', 'is not',
+bin_operators = ('==', '=', '+=', '-=', '*=', '/=',
+                 '<=', '>=', '<', '>'
+                 'not in', 'in', 'is not', 'is',
                  'and', 'or', 'not')
 assignment_operators = ('=', '+=', '-=', '*=', '/=')
 punctuation_marks = (',', ';', ':')
@@ -14,7 +14,9 @@ file_name = 'input.txt'
 
 def main():
     f = open(file_name)
-    content = read(f)
+    content = []
+    for line in f:
+        content.append(line)
     f.close()
     errors = search_errors(content)
     for error in errors:
@@ -24,24 +26,23 @@ def main():
 def search_errors(code):
     errors = []
     line_number = 1
-    previous_line = None
     for line in code:
+        if line.isspace():
+            previous_line = ''
+            continue
         line_length = len(line)
-        if is_start_with_tab(line):
-            errors.append(
-                Error((1, line_number), 'Use spaces instead of tabs'))
-        if is_space_count_multiple_four(line):
+        if not is_space_count_multiple_four(line):
                 errors.append(
                     Error((1, line_number),
                           'Indentation is not a multiple of four'))
-        words = line.split(' ')
+        words = line.split()
         if words[0] == 'class':
-            if not is_cap_word(words[0]):
+            if not is_cap_word(words[1]):
                 errors.append(
                     Error((str.find(line, words[0]), line_number),
                           'Wrong name of class'))
         elif words[0] == 'def':
-            if words[1].isupper():
+            if not words[1].islower():
                 errors.append(
                     Error((str.find(line, words[0]), line_number),
                           'Wrong name of function'))
@@ -81,26 +82,28 @@ def search_errors(code):
                     if line[i-1] == ' ' or line[i+1] == ' ':
                         errors.append(Error((i, line_number),
                                             'unexpected spaces around keyword / parameter equals'))
-        else:
-            for operator in bin_operators:
-                i = line.find(operator)
-                if i != -1:
-                    if (line[i-1] != ' ' or line[i+1] != ' ') and\
-                            not line[i-1].isalpha() and\
-                            not line[i+1].isalpha():
-                        errors.append(Error((i, line_number),
-                                            'missing whitespace around operator'))
-                    if line[i-2] == ' ' or line[i+2] == ' ':
-                        errors.append(Error((i, line_number),
-                                            'multiple spaces around operator'))
+
+        #  В стадии разработки
+        #  else:
+        #      for operator in bin_operators:
+        #          i = line.find(operator)
+        #          if i != -1:
+        #              if (line[i-1] != ' ' or line[i+1] != ' ') and\
+        #                      not line[i-1].isalpha() and\
+        #                      not line[i+1].isalpha():
+        #                  errors.append(Error((i, line_number),
+        #                                      'missing whitespace around operator'))
+        #              if line[i-2] == ' ' or line[i+2] == ' ':
+        #                  errors.append(Error((i, line_number),
+        #                                      'multiple spaces around operator'))
+
         # Не используйте составные инструкции
         for mark in punctuation_marks:
             i = line.find(mark)
             if mark == ',':
                 i = -1
             if i != -1:
-                if words[0] in start_words:
-                    if line[i+1] != '\n':
+                if i+1 < len(line.rstrip()):
                         errors.append(Error((i, line_number),
                                             'multiple statements on one line'))
         # Коментарии
@@ -118,17 +121,12 @@ def search_errors(code):
                 for operator in operators:
                     if line_without_spaces[i-2:i] == operator or\
                        line_without_spaces[i-5:i] == operator:
-                        errors.append(Error((i,line_number),
+                        errors.append(Error((i, line_number),
                                             'Do not compare logical types through operators "==", "!=", "is", "is not"'))
         if line_length > 79:
             errors.append(Error((79, line_number),
                                 'Symbols in line over than 79'))
         line_number += 1
-        previous_line = line
-    last_line = previous_line
-    if last_line != '\n':
-        errors.append(Error((len(last_line), line_number),
-                            'No new line in the end of file'))
     return errors
 
 
@@ -140,27 +138,20 @@ def is_start_with_tab(line):
 def is_space_count_multiple_four(line):
     space_count = 0
     if line[0] == ' ':
-        space_count += 1
         for symbol in line:
             if symbol == ' ':
                 space_count += 1
             else:
                 break
-        if space_count % 4 == 0:
-            return True
+    if space_count % 4 == 0:
+        return True
     return False
 
 
 def is_cap_word(word):
-    if word.istitle() or word.find('_') != -1:
+    if word[0].istitle() and word.find('_') == -1:
         return True
-
-
-def get_file_content(file_name):
-    file = open(file_name)
-    content = read(file)
-    file.close()
-    return content
+    return False
 
 
 class Error():
@@ -170,6 +161,9 @@ class Error():
 
     def write(self):
         print(str(self.coordinates) + ':' + self.description)
+
+    def get_description(self):
+        return self.description
 
 
 if __name__ == '__main__':
