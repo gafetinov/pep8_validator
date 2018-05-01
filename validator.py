d0e61cded98dd92c85f8f887a33aa56c599325c3
Artem Gafetinov <artem.gafetinov@gmail.com>
2018-05-01 11:33:37 +0500

    add codes of errors

--- validator.py
+++ validator.py
@@ -1,5 +1,6 @@
 import sys
 import argparse
+from errors.error import Error
 
 BIN_IPERATORS = ('==', '=', '+=', '-=', '*=', '/=',
                  '<=', '>=', '<', '>'
@@ -12,7 +13,7 @@ OPENED_BRACKETS = ('(', '[', '{')
 CLOSED_BRACKETS = (')', ']', '}')
 BRACKETS = OPENED_BRACKETS + CLOSED_BRACKETS
 START_WORDS = ('def', 'class', 'if', 'while', 'for')
-FILE_NAME = 'input.txt'
+ERRORS = []
 
 
 def main():
@@ -21,56 +22,57 @@ def main():
                         help='Check transferred files to PEP8')
     parser.add_argument('string', nargs='*')
     arguments = parser.parse_args()
-    errors = []
     if arguments.files:
-        for file in arguments.files:
-            with open(file) as file:
+        for file_name in arguments.files:
+            with open(file_name) as file:
+                line_number = 1
                 for line in file:
-                    errors.append(search_errors(line))
+                    search_errors(line, file_name, line_number)
+                    line_number += 1
     else:
-        errors + search_errors(' '.join(arguments.string))
-    for error in errors:
+        search_errors(' '.join(arguments.string), 'string', 1)
+    for error in ERRORS:
         error.write()
 
 
-def search_errors(line):
-    errors = []
-    line_number = 1
-
-    if line.isspace():
-        previous_line = ''
-    line_length = len(line)
+def search_errors(line, file_name, line_number):
     if not is_space_count_multiple_four(line):
-            errors.append(
-                Error((1, line_number),
-                      'Indentation is not a multiple of four'))
+            ERRORS.append(
+                Error(file_name,
+                      (1, line_number),
+                      'E0101'))
     words = line.split()
     if words[0] == 'class':
         if not is_cap_word(words[1]):
-            errors.append(
-                Error((line.find(words[0]), line_number),
-                      'Wrong name of class'))
+            ERRORS.append(
+                Error(file_name,
+                      (line.find(words[1])+1, line_number),
+                      'E0201'))
     elif words[0] == 'def':
         if not words[1].islower():
-            errors.append(
-                Error((line.find(words[0]), line_number),
-                      'Wrong name of function'))
+            ERRORS.append(
+                Error(file_name,
+                      (line.find(words[1])+1, line_number),
+                      'E0202'))
         # Не используйте пробелы вокруг знака =,
         # если он используется для обозначения именованного аргумента
         # или значения параметров по умолчанию.'''
-        for i in range(line_length):
+        for i in range(len(line)):
             if line[i] == '=':
                 if line[i-1] == ' ' or line[i+1] == ' ':
-                    errors.append(Error((i, line_number),
-                                        'Unexpected spaces around keyword / parameter equals'))
+                    ERRORS.append(Error(file_name, (i, line_number),
+                                        ''))
     elif words[0] == 'import':
         if words[1].isupper():
-            errors.append(
-                Error((line.find(words[0]), line_number),
-                      'Wrong name of module'))
+            ERRORS.append(
+                Error(file_name,
+                      (line.find(words[1]+1), line_number),
+                      'E0203'))
         if words[1].find(',') != -1:
-            errors.append(Error((1, line_number),
-                                'Every import should be on a separate line'))
+            ERRORS.append(Error(file_name,
+                                (line.find(words[0])+words[1].find(',')+1,
+                                 line_number),
+                                'E0301'))
     # Избегайте лишних пробелов вокруг скобок и знаков препинания
     previous_word = ''
     next_word = ''
@@ -79,36 +81,45 @@ def search_errors(line):
             next_word = words[i+1]
         if words[i][-1] in OPENED_BRACKETS and\
                 i+1 < len(words) and next_word not in BIN_IPERATORS:
-            errors.append(Error((1, line_number),
-                                'whitespace after ' + words[i][-1]))
+            init_error = {
+                '(': 'E0701',
+                '[': 'E0704',
+                '{': 'E0707',
+            }
+            ERRORS.append(Error(file_name,
+                                (i+1, line_number),
+                                init_error[words[i][-1]]))
         if words[i][0] in BRACKETS+PUNCTUATION_MARKS and\
                 len(previous_word) > 0 and previous_word[-1] != ',' and\
                 previous_word not in BIN_IPERATORS:
-            errors.append(Error((1, line_number),
-                                'whitespace before ' + words[i][0]))
+            init_error = {
+                '(': 'E0702',
+                ')': 'E0703',
+                '[': 'E0705',
+                ']': 'E0706',
+                '{': 'E0708',
+                '}': 'E0709',
+                ',': 'E0710',
+                ':': 'E0711',
+                ';': 'E0712'
+            }
+            ERRORS.append(Error(file_name,
+                                (i+1, line_number),
+                                init_error[words[i][0]]))
         previous_word = words[i]
-    # Не используйте пробелы вокруг знака =,
-    # если он используется для обозначения именованного аргумента
-    # или значения параметров по умолчанию.'''
-    if words[0] == 'def':
-        for i in range(line_length):
-            if line[i] == '=':
-                if line[i-1] == ' ' or line[i+1] == ' ':
-                    errors.append(Error((i, line_number),
-                                        'unexpected spaces around keyword / parameter equals'))
 
     #  В стадии разработки
     #  else:
-    #      for operator in BIN_IPERATORS:
+    #      for operator in BIN_OPERATORS:
     #          i = line.find(operator)
     #          if i != -1:
     #              if (line[i-1] != ' ' or line[i+1] != ' ') and\
     #                      not line[i-1].isalpha() and\
     #                      not line[i+1].isalpha():
-    #                  errors.append(Error((i, line_number),
+    #                  ERRORS.append(Error(file_name, (i, line_number),
     #                                      'missing whitespace around operator'))
     #              if line[i-2] == ' ' or line[i+2] == ' ':
-    #                  errors.append(Error((i, line_number),
+    #                  ERRORS.append(Error(file_name, (i, line_number),
     #                                      'multiple spaces around operator'))
 
     # Не используйте составные инструкции
@@ -118,15 +129,17 @@ def search_errors(line):
             i = -1
         if i != -1:
             if i+1 < len(line.rstrip()):
-                    errors.append(Error((i, line_number),
-                                        'multiple statements on one line'))
+                    ERRORS.append(Error(file_name,
+                                        (i, line_number),
+                                        'E0302'))
     # Коментарии
     line = line.lstrip()
     i = line.find('#')
     if i != -1:
         if line[i+1] != ' ':
-            errors.append(Error((i, line_number),
-                                'inline comment should start with "# "'))
+            ERRORS.append(Error(file_name,
+                                (i, line_number),
+                                'E0401'))
     # Не сравнивайте логические типы через ==
     for boolean in BOOL_TYPES:
         line_without_spaces = line.replace(' ', '')
@@ -136,13 +149,21 @@ def search_errors(line):
             for operator in operators:
                 if line_without_spaces[i-2:i] == operator or\
                    line_without_spaces[i-5:i] == operator:
-                    errors.append(Error((i, line_number),
-                                        'Do not compare logical types through operators "==", "!=", "is", "is not"'))
-    if line_length > 79:
-        errors.append(Error((79, line_number),
-                            'Symbols in line over than 79'))
-    line_number += 1
-    return errors
+                    ERRORS.append(Error(file_name,
+                                        (i, line_number),
+                                        'E0501'))
+    if len(line) > 79:
+        ERRORS.append(Error(file_name,
+                            (79, line_number),
+                            'E0601'))
+
+
+def get_error07(symbol, before=True):
+    if before:
+        if symbol == '(':
+            return 'E702'
+        elif symbol == ')':
+            return
 
 
 def is_start_with_tab(line):
@@ -157,18 +178,6 @@ def is_space_count_multiple_four(line):
 
 def is_cap_word(word):
     return word[0].istitle() and word.find('_') == -1
-
-
-class Error:
-    def __init__(self, coordintes, description):
-        self.coordinates = coordintes
-        self.description = description
-
-    def write(self):
-        print('{0}:{1}'.format(self.coordinates, self.description))
-
-    def get_description(self):
-        return self.description
 
 
 if __name__ == '__main__':
