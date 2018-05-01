e864bc5cac6bf276b65535623ec2353708ada978
Artem Gafetinov <artem.gafetinov@gmail.com>
2018-05-01 11:34:12 +0500

    add codes of errors

---
+++ errors/error.py
@@ -0,0 +1,22 @@
+class Error:
+    def __init__(self, file_name, coordintes, err_code):
+        self.file_name = file_name
+        self.coordinates = coordintes
+        self.err_code = err_code
+        self.language = 'english'
+        self.languages = ('english')
+
+    def write(self):
+        print('On {0} {1}: {2}'.
+              format(self.file_name, self.coordinates, self.description()))
+
+    def description(self):
+        with open('errors/{}.txt'.format(self.language)) as file:
+            for line in file:
+                if line.find(self.err_code) != -1:
+                    dates = line.split('-')
+                    return dates[1]
+
+    def change_language(self, language):
+        if language in self.languages:
+            self.language = language
